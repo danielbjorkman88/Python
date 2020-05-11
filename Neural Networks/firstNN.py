3e80516481302f92e1f84efbdf0aa07b81298ad2
Daniel <mysgubbe@gmail.com>
2020-05-11 19:42:40 +0200

    update

--- Neural Networks/firstNN.py
+++ Neural Networks/firstNN.py
@@ -20,7 +20,20 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-plt.imshow(train_images[0], cmap = plt.cm.binary)
-plt.show()
+
+model = keras.Sequential([
+        keras.layers.Flatten(input_shape = (28,28)),
+        keras.layers.Dense(128 , activation = "relu"),
+        keras.layers.Dense(10, activation ="softmax")
+        ])
+
+model.compile(optimizer ="adam", loss= "sparse_categorical_crossentropy", metrics = ["accuracy"])
+
+model.fit(train_images , train_labels , epochs = 10)
+ 
+
+#
+#plt.imshow(train_images[0], cmap = plt.cm.binary)
+#plt.show()
 
 
